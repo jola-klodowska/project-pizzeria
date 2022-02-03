2a70f1ecfa0a5bd30d75dd5c8dc990a20a598c0a
Jola Klodowska <jola.klodowska@wp.pl>
2022-02-03 18:44:57 +0100

    Add function initActions to class Cart

--- project-pizzeria/src/js/script.js
+++ project-pizzeria/src/js/script.js
@@ -305,6 +305,7 @@
       thisCart.products = [];
 
       thisCart.getElements(element);
+      thisCart.initActions();
 
       console.log('new Cart', thisCart);
     }
@@ -315,6 +316,15 @@
       thisCart.dom = {};
 
       thisCart.dom.wrapper = element;
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    }
+
+    initActions() {
+      const thisCart = this;
+
+      thisCart.dom.toggleTrigger.addEventListener('click', function() {
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
     }
   }
 
